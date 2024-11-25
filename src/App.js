b264e22bd00ec5eb811c6705142ad0c52220d4db
ejke <eikejraielo@gmail.com>
2024-11-25 10:46:47 +0200

    Fetch and show initial data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { fetchMoonDayData } from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [moonData, setMoonData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const data = await fetchMoonDayData(new Date().toISOString());
+                setMoonData(data);
+            } catch (err) {
+                setError("Failed to load moon day data.");
+            }
+        };
+        getData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Moon Days</h1>
+            {error && <p>{error}</p>}
+            {moonData ? (
+                <div>
+                    <h2>{moonData.date}</h2>
+                    <p>Color: {moonData.color}</p>
+                    <p>Symbol: {moonData.symbol}</p>
+                    {/* Display more fields as needed */}
+                </div>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+};
+
 
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://isabelblackwell.pythonanywhere.com';
+const LOCAL_API_BASE_URL = 'http://127.0.0.1:5000';
+
+export const fetchMoonDayData = async (date) => {
+    console.log('date', date)
+    const formattedDate = formatDate(date);
+    console.log('formattedDate', formattedDate)
+    try {
+        const response = await axios.get(`${LOCAL_API_BASE_URL}/moon-day-data?date=${formattedDate}`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching moon day data:", error);
+        throw error;
+    }
+};
+
+const formatDate = (date) => {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const day = String(d.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+};
