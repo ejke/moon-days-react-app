b5a91ab22bed6dcfadda1a429bcd888da93828ac
Eike <eikejraielo@gmail.com>
2025-02-24 21:11:21 +0200

    Add buttons to day view

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # Moon days react app
 
-That is  frontend for the MoonApp.
+That is frontend for the MoonApp.
 Live https://moon-days-react-app.vercel.app/
 The CICD is automatic from origin/main branch. What goes up goes live.
 
 ## Get up and running
+
 ```
 npm start
 ```
 
 ## TODO
+
 1. subs - tasuta jagadad vaid tänast. Maksevõimekus
-5. reklaam A4 qr koodiga, track emaili regajaid
-2. Päevavaade, lisada nupud liigutamiseks küljelt küljele
+2. reklaam A4 qr koodiga, track emaili regajaid
 3. kuuvaade muutus kui liigutada lehte
-2. burger menüü kui väike
-3. Sisselogimine Google & teiste auth meetoditega + email
-Välja logitud vaade
-Sisselogitud vaade, auth jne
+4. burger menüü kui väike
+5. Sisselogimine Google & teiste auth meetoditega + email
+   Välja logitud vaade
+   Sisselogitud vaade, auth jne
--- src/pages/PaevavaadePage.js
+++ src/pages/PaevavaadePage.js
@@ -4,88 +4,134 @@ import { useLocation } from 'react-router-dom';
 import { fetchMoonDayData } from '../services/api';
 import { formatDate, formatTime } from '../services/format';
 import '../App.css';
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const PaevavaadePage = () => {
-    const location = useLocation();
-    const queryParams = new URLSearchParams(location.search);
-    const selectedDate = queryParams.get('date');
-    const initialDate = selectedDate ? new Date(selectedDate) : new Date();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const selectedDate = queryParams.get('date');
+  const initialDate = selectedDate ? new Date(selectedDate) : new Date();
 
-    const [moonData, setMoonData] = useState([]);
-    const [error, setError] = useState(null);
-    const [date, setDate] = useState(initialDate);
+  const [moonData, setMoonData] = useState([]);
+  const [error, setError] = useState(null);
+  const [date, setDate] = useState(initialDate);
 
-    useEffect(() => {
-        const getData = async (selectedDate) => {
-            try {
-                const data = await fetchMoonDayData(selectedDate.toISOString().split('T')[0]);
-                setMoonData(data);
-                if (Array.isArray(data) && data.length > 0) {
-                    document.documentElement.style.setProperty('--hex-color-1', data[0].hex);
-                    if (data.length > 1) {
-                        document.documentElement.style.setProperty('--hex-color-2', data[1].hex);
-                    } else {
-                        document.documentElement.style.setProperty('--hex-color-2', data[0].hex);
-                    }
-                }
-            } catch (err) {
-                setError("Failed to load moon day data.");
-            }
-        };
-        getData(date);
-    }, [date]);
+  useEffect(() => {
+    const getData = async (selectedDate) => {
+      try {
+        const data = await fetchMoonDayData(
+          selectedDate.toISOString().split('T')[0]
+        );
+        setMoonData(data);
+        if (Array.isArray(data) && data.length > 0) {
+          document.documentElement.style.setProperty(
+            '--hex-color-1',
+            data[0].hex
+          );
+          if (data.length > 1) {
+            document.documentElement.style.setProperty(
+              '--hex-color-2',
+              data[1].hex
+            );
+          } else {
+            document.documentElement.style.setProperty(
+              '--hex-color-2',
+              data[0].hex
+            );
+          }
+        }
+      } catch (err) {
+        setError('Failed to load moon day data.');
+      }
+    };
+    getData(date);
+  }, [date]);
 
-    return (
-        <div className='bg bg-color'>
-            <section>
-                <div className="datepicker">
-                    <DatePicker 
-                        dateFormat="dd/MM/yyyy" 
-                        selected={date} 
-                        onChange={(date) => setDate(date)} 
-                        calendarStartDay={1} // This sets the week to start on Monday
-                    />
-                </div>
-            
-                    {error && <p>{error}</p>}
-                    {moonData.length > 0 ? (
-                        moonData.map((item, index) => (
-                            <div className="box">
-                                <div>
-                                    <p className='smol'>{formatDate(item.date)}  🚀🌙 Kuutõus: {formatTime(item.moonrise_time)}</p>
-                                    <h1 className='headline'>{item.moon_date}. {item.symbol} {item.emoji}</h1>
-                                    <p className='after-headline'>
-                                        <span className='description'>Värv: </span>
-                                        <span className={`color-${index}`} >{item.color}</span>
-                                        <span className='description'> Element: </span>
-                                        {item.element}
-                                    </p>
-                                    <p>{item.keywords}</p>
-                                    <p>{item.comment}</p>
-                                    <div className='characteristics'>
+  const handlePreviousDay = () => {
+    const newDate = new Date(date);
+    newDate.setDate(date.getDate() - 1);
+    setDate(newDate);
+  };
 
-                                        <p><span className='description'>Tegevus:</span> {item.activity}</p>
-                                        <p><span className='description'>Vägi:</span> {item.power}</p>
-                                        <p><span className='description'>Tervis:</span> {item.health}</p>
-                                        <p><span className='description'>Ended:</span> {item.omens}</p>
-                                        <p><span className='description'>Rituaalid:</span> {item.rituals}</p>
+  const handleNextDay = () => {
+    const newDate = new Date(date);
+    newDate.setDate(date.getDate() + 1);
+    setDate(newDate);
+  };
 
-                                        {/* Display more fields as needed */}
-                                    </div>
-                                    <p>{item.comment_2}</p>
-                                </div>
-                            </div>
-                        ))
-                    ) : (
-                        <div className="box">
-                            <p>Laeb...</p>
-                        </div>
-                    )}
-            </section>
+  return (
+    <div className='bg bg-color'>
+      <section>
+        <div className='datepicker-container'>
+          <button onClick={handlePreviousDay} className='date-nav-button'>
+            ←
+          </button>
+
+          <DatePicker
+            dateFormat='dd/MM/yyyy'
+            selected={date}
+            onChange={(date) => setDate(date)}
+            calendarStartDay={1} // This sets the week to start on Monday
+          />
+          <button onClick={handleNextDay} className='date-nav-button'>
+            →
+          </button>
         </div>
-    );
+
+        {error && <p>{error}</p>}
+        {moonData.length > 0 ? (
+          moonData.map((item, index) => (
+            <div className='box'>
+              <div>
+                <p className='smol'>
+                  {formatDate(item.date)} 🚀🌙 Kuutõus:{' '}
+                  {formatTime(item.moonrise_time)}
+                </p>
+                <h1 className='headline'>
+                  {item.moon_date}. {item.symbol} {item.emoji}
+                </h1>
+                <p className='after-headline'>
+                  <span className='description'>Värv: </span>
+                  <span className={`color-${index}`}>{item.color}</span>
+                  <span className='description'> Element: </span>
+                  {item.element}
+                </p>
+                <p>{item.keywords}</p>
+                <p>{item.comment}</p>
+                <div className='characteristics'>
+                  <p>
+                    <span className='description'>Tegevus:</span>{' '}
+                    {item.activity}
+                  </p>
+                  <p>
+                    <span className='description'>Vägi:</span> {item.power}
+                  </p>
+                  <p>
+                    <span className='description'>Tervis:</span> {item.health}
+                  </p>
+                  <p>
+                    <span className='description'>Ended:</span> {item.omens}
+                  </p>
+                  <p>
+                    <span className='description'>Rituaalid:</span>{' '}
+                    {item.rituals}
+                  </p>
+
+                  {/* Display more fields as needed */}
+                </div>
+                <p>{item.comment_2}</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className='box'>
+            <p>Laeb...</p>
+          </div>
+        )}
+      </section>
+    </div>
+  );
 };
 
 export default PaevavaadePage;
